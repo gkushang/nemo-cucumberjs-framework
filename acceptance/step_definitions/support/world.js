e74f1cd76c9af42ba93f74fc4ccb30d33901ac24
kgajjar <kgajjar@salesforce.com>
2017-06-28 12:50:21 -0400

    clean up the world

--- acceptance/step_definitions/support/world.js
+++ acceptance/step_definitions/support/world.js
@@ -1,11 +1,11 @@
 var {defineSupportCode} = require('cucumber');
 var Promise = require('bluebird');
 var Nemo = require('nemo');
-var configuration = require('../config/utils/configuration');
+var configuration = require('../../config/utils/configuration');
 var nemoPage = require('nemo-pageobjects').nemoPage;
-var sauceConfig = require('../config/sauce.json');
-var commonConfig = require('../config/config.json');
-var Keys = require('../config/utils/keys');
+var sauceConfig = require('../../config/sauce.json');
+var commonConfig = require('../../config/config.json');
+var Keys = require('../../config/utils/keys');
 var replicate = require('cucumber-replicate').replicate;
 
 function CustomWorld({attach}) {
